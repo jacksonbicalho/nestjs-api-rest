e78570ecd93c9f3bb98bb4924511e52f32d8d742
Jackson Bicalho <jacksonbicalho@gmail.com>
2021-01-25 11:59:54 -0300

    Arrumando mais um pouco

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import express from 'express';
-import { verify, sign, decode, SignOptions } from 'jsonwebtoken';
+import { verify, sign, decode, SignOptions, VerifyOptions } from 'jsonwebtoken';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
@@ -11,6 +11,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     algorithm: 'HS256',
     expiresIn: 300,
   }
+
+  private verifyOptions: VerifyOptions = { }
+
 
   constructor(){
     super({passReqToCallback: true});
@@ -30,12 +33,32 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     if (bearer[0] != 'bearer' && bearer[0] != 'Bearer'  ) {
       throw new UnauthorizedException('not found bearer in authorization #2');
     }
-    const tokenCripto = bearer[1]
-    let decoded = verify(tokenCripto, 'shhhhh');
-    // verify()
 
-    const id = 1; //esse id viria do banco de dados
-    const token = sign({ id }, "shhhhh", this.signOptions);
-    return { auth: true, token: token };
+    const tokenCripto = bearer[1];
+
+    verify(tokenCripto, 'shhhhh', function(err, decoded) {
+
+      if (err) {
+        // https://www.npmjs.com/package/jsonwebtoken#jsonwebtokenerror
+        // 'jwt malformed'
+        // 'jwt signature is required'
+        // 'invalid signature'
+        // 'jwt audience invalid. expected: [OPTIONS AUDIENCE]'
+        // 'jwt issuer invalid. expected: [OPTIONS ISSUER]'
+        // 'jwt id invalid. expected: [OPTIONS JWT ID]'
+        // 'jwt subject invalid. expected: [OPTIONS SUBJECT]'
+        if (err.name == 'JsonWebTokenError' ) {
+          throw new UnauthorizedException(err.message);
+        }
+      }
+    });
+
+    // const tokenCripto = bearer[1]
+    // let decoded = verify(tokenCripto, 'shhhhh', this.verifyOptions);
+    // return decoded;
+
+    // const id = 1; //esse id viria do banco de dados
+    // const token = sign({ id }, "shhhhh", this.signOptions);
+    // return { auth: true, token: token };
   }
 }
